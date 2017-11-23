ee007ddc1186c3edf303662a729c93d5c4101877
Jessica Rodriguez <jr@mettrr.com>
2017-11-23 11:01:11 +0000

    build path to view single threads

--- tests/Feature/ThreadsTest.php
+++ tests/Feature/ThreadsTest.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 class ThreadsTest extends TestCase
 {
     use DatabaseMigrations;
+
     /** @test */
     public function a_user_can_view_all_threads()
     {
@@ -16,6 +17,14 @@ class ThreadsTest extends TestCase
         $response = $this->get('/threads');
 
         $response->assertSee($thread->title);
+
+
+    }
+
+    /** @test */
+    function a_user_can_read_a_single_thread()
+    {
+        $thread = factory('App\Thread')->create();
 
         $response = $this->get('/threads/' . $thread->id);
 
