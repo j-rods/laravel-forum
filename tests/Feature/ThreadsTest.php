3763d271913d7d562afd57357208dc5fb3981757
Jessica Rodriguez <jr@mettrr.com>
2017-11-30 11:28:07 +0000

    a user may see threads

--- tests/Feature/ThreadsTest.php
+++ tests/Feature/ThreadsTest.php
@@ -9,14 +9,18 @@ class ThreadsTest extends TestCase
 {
     use DatabaseMigrations;
 
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->thread = factory('App\Thread')->create();
+    }
+
     /** @test */
     public function a_user_can_view_all_threads()
     {
-        $thread = factory('App\Thread')->create();
-
-        $response = $this->get('/threads');
-
-        $response->assertSee($thread->title);
+        $response = $this->get('/threads')
+                    ->assertSee($this->thread->title);
 
 
     }
@@ -24,10 +28,17 @@ class ThreadsTest extends TestCase
     /** @test */
     function a_user_can_read_a_single_thread()
     {
-        $thread = factory('App\Thread')->create();
+        $response = $this->get('/threads/' . $this->thread->id)
+                    ->assertSee($this->thread->title);
+    }
 
-        $response = $this->get('/threads/' . $thread->id);
+    /** @test */
+    function a_user_can_read_replies_that_are_associated_with_a_thread()
+    {
+        $reply = factory('App\Reply')
+               ->create(['thread_id' => $this->thread->id]);
 
-        $response->assertSee($thread->title);
+        $this->get('/threads/' . $this->thread->id)
+              ->assertSee($reply->body);
     }
 }
